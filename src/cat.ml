0500cb211511417c0134284fd7b8c48b7a1b7aac
Artem Titoulenko <artem.titoulenko@gmail.com>
2018-01-05 15:41:51 -0500

    initial commit

---
+++ src/cat.ml
@@ -0,0 +1,26 @@
+open Core.Std
+open Command.Let_syntax
+
+let printHelp () = "Usage: cat [--help] <file>";;
+
+let printFile (filePath) =
+  match Sys.file_exists_exn filePath with
+    | true ->
+        In_channel.with_file filePath ~f:(fun file ->
+          In_channel.iter_lines file print_endline
+        )
+    | false ->
+      Printf.printf "%s does not exist" filePath
+  ;;
+
+let () =
+  Command.basic'
+    ~summary:"A unix cat clone"
+    [%map_open
+      let filename =
+        anon ("<file>" %: string)
+      in
+      fun () ->
+        printFile filename;
+    ]
+  |> Command.run ~version:"1.0" ~build_info:"RWO";;
